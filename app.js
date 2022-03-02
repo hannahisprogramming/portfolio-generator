286c43459ef4ddba99fcc2e2564866d676905291
Hannah Bush <heb.rutgers@gmail.com>
2022-03-02 18:50:35 -0500

    user input successfully captured

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+// Include process module
+const process = require('process');
+  
+// Printing process.argv property value
+var profileItem = process.argv;
+
 const printProfileData = profileDataArr => {
   profileDataArr.forEach(profileItem => console.log(profileItem));
 };
