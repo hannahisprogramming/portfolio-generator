f319771b4b64d99578904265b872119fa505e0e8
Hannah Bush <heb.rutgers@gmail.com>
2022-03-07 20:54:51 -0500

    about section functioning

--- app.js
+++ app.js
@@ -13,6 +13,20 @@ const generatePage = require('./src/page-template');
 
 const inquirer = require('inquirer');
 
+// create the about section
+const generateAbout = aboutText => {
+  if (!aboutText) {
+    return '';
+  }
+
+  return `
+    <section class="my-3" id="about">
+      <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
+      <p>${aboutText}</p>
+    </section>
+  `;
+};
+
 const promptUser = () => {
   return inquirer.prompt([
     {
